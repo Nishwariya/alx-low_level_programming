e12f05af7a2b6e7683ccd14faea2cb4be8e13e48
Nishwariya <nishwariya01@gmail.com>
2022-10-31 07:56:41 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,38 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * read_iletext - reads a text file and prints it to the POSIX standard output
- *
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- *
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ * function should read and print.
+ * Return: If the function fails or filename is NULL - 0.
+ * O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	ssize_t op, rd, wr;
 	char *buffer;
-	int rc, wc; /* read &write character counter */
-	int fp; /* File descriptor */
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	fp = open(filename, 0_RDONLY);
-	if (fp < 0)
-		return (0);
-
-	buffer = malloc(letters * sizeof(char));
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	rc = read(fp, buffer, letters);
-	if (rc < 0)
+	op = open(filename, O_RDONLY);
+	rd = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, rd);
+
+	if (op == -1 || rd == -1 || wr == -1 || wr != rd)
 	{
 		free(buffer);
 		return (0);
 	}
-	buffer[rc] = '\0';
-	close(fp);
 
-	wc = write(STDOUT_FILENO, buffer, rc);
 	free(buffer);
+	close(op);
 
-	if (wc < 0)
-		return (0);
-
-	return (wc);
+	return (wr);
 }
