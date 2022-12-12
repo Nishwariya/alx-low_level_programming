81c54da1572810344e7fa94537204cf2161323aa
Nishwariya <nishwariya01@gmail.com>
2022-12-12 11:28:48 -0800

    commited

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _islower -> check if the character is lowercase
+ * @c: a character argument
+ * Return: returns 1 and zero depending on condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
